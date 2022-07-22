3321ad111d36aca61240c35574dd1ba1308db753
Svetla Yankova <syankova@progress.com>
2022-07-22 14:35:24 +0300

    SignalR Chat application

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat_msgpack.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
